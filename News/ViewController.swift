81b85eb0bf9eac1628e796b337cfe449a9a94199
 Aitolkun Anarbekova <tokusha.aa@gmail.com>
2024-10-20 15:45:20 +0600

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Айтолкун Анарбекова on 20.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
